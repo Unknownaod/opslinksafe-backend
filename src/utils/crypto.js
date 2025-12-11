861dff5ae43fc676d28fbd75f2bb7026accca218
Unknownaod <unknown@dona.pw>
2025-12-11 16:09:58 -0500

    Create crypto.js

---
+++ src/utils/crypto.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { env } from "../config/env.js";
+
+export async function hashPassword(plain) {
+  const salt = await bcrypt.genSalt(env.BCRYPT_ROUNDS);
+  return bcrypt.hash(plain, salt);
+}
+
+export async function comparePassword(plain, hash) {
+  return bcrypt.compare(plain, hash);
+}
+
+export function signJwt(payload) {
+  return jwt.sign(payload, env.JWT_SECRET, {
+    expiresIn: env.JWT_EXPIRES_IN
+  });
+}
+
+export function verifyJwt(token) {
+  return jwt.verify(token, env.JWT_SECRET);
+}
